dfc83633d1abc72e38ee843d9bd1c3c656155c7c
ThiagoGs-Dev <96384024+ThiagoGs-Dev@users.noreply.github.com>
2023-05-20 16:52:52 -0300

    Correção do Efeito Parallax

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = ({Copyright, Author}) => {
     return(
         <div className='bg-[#172554] flex justify-center items-center w-full h-full text-white p-5'>
             <div className='flex justify-center items-center flex-col'>
-                <h3 className='text-1xl' >{ Copyright }</h3>
+                <h3 className='text-1xl text-center' >{ Copyright }</h3>
                 <p>{ Author }</p>
             </div>
         </div>
